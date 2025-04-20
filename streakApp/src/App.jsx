8a361cbe1b1153223798d4b1abb3cdbaefb64225
Dimmsum <153735532+Dimmsum@users.noreply.github.com>
2025-04-20 23:08:34 +0000

    setup pages

--- streakApp/src/App.jsx
+++ streakApp/src/App.jsx
@@ -1,12 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Landing';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
-
-function App(){
-  return(
-    <>
-    <h1>App</h1>
-    </>
+function App() {
+  return (
+    // React router setup to enable routing between different pages
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
   );
 }
-export default App
+
+export default App;
